6c53fef38325a1333b5c79dc18573f9fb6f02c9a
youngsuen <youngsuen@ximalaya.com>
2019-01-09 19:07:41 +0800

    add weights

---
+++ aec/post_process/aec_post_net.h
@@ -0,0 +1,12 @@
+#ifndef _AEC_POST_NET_H_
+#define _AEC_POST_NET_H_
+
+extern float W_DenseLayer1[512][555];
+extern float W_DenseLayer2[256][512];
+extern float W_DenseLayer3[256][256];
+extern float W_DenseLayer4[185][256];
+extern float W_DenseLayer1_Bias[512];
+extern float W_DenseLayer2_Bias[256];
+extern float W_DenseLayer3_Bias[256];
+extern float W_DenseLayer4_Bias[185];
+#endif
