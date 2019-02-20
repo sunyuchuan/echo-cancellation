3e2995416cff5f615ebe9848dd829272f856120c
Layne.huang <layne.huang@ximalaya.com>
2019-02-20 10:23:43 +0800

    修改CMakeLists.txt

--- utility/log/log.h
+++ utility/log/log.h
@@ -1,15 +1,7 @@
 #ifndef _AEC_LOG_H_
 #define _AEC_LOG_H_
 
-#ifdef DEBUG_ON_LINUX
-
-#define LOGD printf
-#define LOGI printf
-#define LOGW printf
-#define LOGE printf
-#define LOGF printf
-
-#else
+#ifdef __ANDROID__
 #include <android/log.h>
 
 #define TAG "debug-AEC"
@@ -18,6 +10,12 @@
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, TAG, __VA_ARGS__)
+#elif __linux__
+#define LOGD printf
+#define LOGI printf
+#define LOGW printf
+#define LOGE printf
+#define LOGF printf
 #endif
 
 #endif
